290aeef26e8f6eba51c3c91dceab00dcd85ee16d
Oleksandr Yovbak <oleksandr.yovbak@gmail.com>
2024-05-30 01:22:45 +0300

    Fix project

--- main.py
+++ main.py
@@ -4,14 +4,16 @@ from pathlib import Path
 from git.repo import Repo
 
 
-N = 100
+N = 200
 repo = Repo(Path(__file__).parent.absolute())
 path = Path("text.txt")
 
-with path.open("a") as file:
-    repo.index.add(path)
-    for i in range(N):
+repo.index.add([path])
+for i in range(N):
+    with path.open("a") as file:
         file.write("*")
-        repo.index.commit(f"Commit: {datetime.now()} {i}")
+    repo.index.commit(f"Commit: {datetime.now()} {i}")
+
+with path.open("a") as file:
     file.truncate(0)
     repo.index.commit(f"Last Commit: {datetime.now()}")
